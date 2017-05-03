8bc0dfca3ea7d308b3a55bf13622117eddeb1d9d
em7 <mm007.emko@gmail.com>
2017-05-03 22:10:52 +0200

    Added funtions for generating board

--- src/mpegthing/core.clj
+++ src/mpegthing/core.clj
@@ -35,6 +35,67 @@
 
 ;; page 114, defn connect
 
+(defn connect
+  "Form a mutual connection between two positions"
+  [board max-pos pos neighbor destination]
+  (if (> destination max-pos)
+    ;; don't modify the board for connections which would lead
+    ;; out of the board
+    board
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbor))
+            board
+            [[pos destination] [destination pos]]))) 
+
+;; The board map should look like this:
+;; {1 {:pegged true, :connections {6 3, 4 2}}, ;; 1 is connected to 6 through 3 and with 4 through 2
+;; ...,
+;; :rows 5}
+
+(defn connect-right
+  [board max-pos pos]
+  (let [neighbor      (inc pos)
+        destination   (inc neighbor)]
+    ;; make connection only when there is a place to the right
+    ;; if neighbor or pos are triangular, it means they are at the edge
+    (if-not (or (triangular? neighbor) (triangular? pos))
+      (connect board max-pos pos neighbor destination)
+      board)))
+
+(defn connect-down-left
+  [board max-pos pos]
+  (let [row          (row-num pos)
+        neighbor     (+ row pos)
+        destination  (+ 1 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+(defn connect-down-right
+  [board max-pos pos]
+  (let [row          (row-num pos)
+        neighbor     (+ 1 row pos)
+        destination  (+ 2 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+(defn add-pos
+  "Pegs the position and perform connections"
+  [board max-pos pos]
+  (let [pegged-board (assoc-in board [pos :pegged] true)]
+    (reduce (fn [new-board connection-creation-fn]
+              (connection-creation-fn new-board max-pos pos))
+            pegged-board
+            [connect-right connect-down-left connect-down-right])))
+
+(defn new-board
+  "Creates a new board with the given number of rows"
+  [rows]
+  (let [initial-board {:rows rows}
+        max-pos (row-tri rows)]
+    (reduce (fn [board pos] (add-pos board max-pos pos))
+            initial-board
+            (range 1 (inc max-pos)))))
+
+;; page 117 (144) Moving Pegs
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
