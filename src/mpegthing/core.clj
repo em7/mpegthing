b3dbe383fdf549253b152b24db2e32c76cd45efa
em7 <mm007.emko@gmail.com>
2017-05-05 22:38:41 +0200

    Finished

--- src/mpegthing/core.clj
+++ src/mpegthing/core.clj
@@ -5,7 +5,12 @@
             [clojure.string :as string])
   (:gen-class))
 
-(declare successful-move prompt-move game-over query-rows)
+(declare user-entered-valid-move
+         user-entered-invalid-move
+         prompt-move
+         game-over
+         prompt-rows
+         prompt-empty-peg)
 
 (defn tri*
   "Generates lazy sequence of triangular numbers."
@@ -183,11 +188,88 @@
   (doseq [row-num (range 1 (inc (:rows board)))]
     (println (render-row board row-num))))
 
+;; 121 (149) Player Interaction
+
+(defn letter->pos
+  "Converts a letter string to the corresponding position number."
+  [letter]
+  (inc (- (int (first letter)) alpha-start)))
+
+(defn characters-as-strings
+  "Returns the characters (letters) as strings from the input string.
+  E.g. for input \"a     cb\" returns (\"a\" \"c\" \"b\")"
+  [s]
+  (re-seq #"[a-zA-Z]" s))
+
+(defn get-input
+  "Waits for user to enter text and hit enter, then cleans the input."
+  ([] (get-input nil))
+  ([default]
+   (let [input (string/trim (read-line))]
+     (if (empty? input)
+       default
+       (string/lower-case input)))))
+
+(defn prompt-move
+  [board]
+  (println "\nHere's your board:")
+  (render-board board)
+  (println "Move from where to where? Enter two letters:")
+  
+  (let [input (map letter->pos (characters-as-strings (get-input)))]
+    (if-let [new-board (make-move board (first input) (second input))]
+      (user-entered-valid-move new-board)
+      (user-entered-invalid-move board))))
+
+(defn user-entered-invalid-move
+  "Handles the next step after a user has entered an invalid move."
+  [board]
+  (println "\n!!! That was an invalid move\n")
+  (prompt-move board))
+
+(defn user-entered-valid-move
+  "Handles the next step after the user has entered a valid move."
+  [board]
+  (if (can-move? board)
+    (prompt-move board)
+    (game-over board)))
+
+(defn game-over
+  "Announce the game is over and prompt to play again."
+  [board]
+  (let [remaining-pegs (count (filter :pegged (vals board)))]
+    (println "Game over! You had" remaining-pegs "pegs left:")
+    (render-board board)
+    (println "Play again? y/n [y]")
+    (let [input (get-input "y")]
+      (if (= "y" input)
+        (prompt-rows)
+        (do
+          (println "Bye!")
+          (System/exit 0))))))
+
+(defn prompt-rows
+  []
+  (println "How many rows? [5]")
+  (let [rows   (Integer. (get-input 5))
+        board  (new-board rows)]
+    (prompt-empty-peg board)))
+
+(defn prompt-empty-peg
+  [board]
+  (println "Here's your board:")
+  (render-board board)
+  (println "Remove which peg? [e]")
+  (prompt-move (remove-peg board (letter->pos (get-input "e")))))
+
 ;; (def board (atom (new-board 5)))
 ;; (swap! board #(remove-peg % 4))
-       
+;; (render-board @board)
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Get ready to play peg thing!")
+  (prompt-rows))
+       
+
 
